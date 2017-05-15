83120c84e9b33273dc7ff7959619255bb0593b6f
Ramin Lavang <ramin@trisentry.com>
2017-05-15 15:05:03 -0400

    Create ex6.rb

---
+++ 100-back-end-prep/introduction-to-programming/chapter4-flow-control/ex6.rb
@@ -0,0 +1,2 @@
+#Another end is required to close off the method. Currently one end keyword is being used to close off the if/else statement but 
+#not the function definition. Another end keyword after the current end will fix the problem.
