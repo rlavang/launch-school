6d2d6bf406a7b08e5c9bc2c8897b598d59849952
Ramin Lavang <ramin@trisentry.com>
2017-05-17 15:30:30 -0400

    Create ex7.rb

---
+++ 100-back-end-prep/introduction-to-programming/chapter6-arrays/chapter7-hashes/ex7.rb
@@ -0,0 +1,6 @@
+#The first hash is based on a new way of creating a hash using a symbol
+#As such, a symbol :x is set to the value of "some value"
+
+#The second hash uses the variable x instead as the key "hi there" with a value of
+#"some value". The old method of creating a hash requires the user to explicitly
+#input the colon for a symbol value
