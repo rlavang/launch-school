b3b209e2a2bf13fc1d5957534a9ef7c674d320b5
Ramin Lavang <ramin@trisentry.com>
2017-05-17 10:12:03 -0400

    Create ex2.rb

---
+++ 100-back-end-prep/introduction-to-programming/chapter6-arrays/ex2.rb
@@ -0,0 +1,5 @@
+#1. The program should return 1, which is the element being deleted from the selected subarray. 
+#   arr should have the value of [["b"], ["b", 2], ["b", 3], ["a", 1], ["a", 2], ["a", 3]] now
+
+#2. the program should return [1,2,3] as this is the last element of the selected array.
+#  arr should now have the value of [["b"], ["a", [1, 2, 3]]]
