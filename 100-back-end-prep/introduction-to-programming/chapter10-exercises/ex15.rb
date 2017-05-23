653cf0b6dfe3651c2b4749da94ba7a09ce8a9869
Ramin Lavang <ramin@trisentry.com>
2017-05-23 14:55:33 -0400

    Update ex15.rb

--- 100-back-end-prep/introduction-to-programming/chapter10-exercises/ex15.rb
+++ 100-back-end-prep/introduction-to-programming/chapter10-exercises/ex15.rb
@@ -1,7 +1,9 @@
-arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
-
 arr.delete_if do |item|
   item.start_with?('s')
+end
+p arr
+arr.delete_if do |item|
+  item.start_with?('s', 'w')
 end
 
 p arr
