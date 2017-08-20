9a527ad4ebe172b3a3914f30916c9d26592f34d3
InstantRamin <ramin@trisentry.com>
2017-08-20 11:28:05 -0400

    Addded 101 lesson

---
+++ 100-back-end-prep/introduction-to-programming/chapter1-the-basics/sample.rb
@@ -0,0 +1,30 @@
+#Ex 1
+#puts "Ramin " + "Lavang"
+
+#Ex 2
+###
+## puts 5639 / 1000 ##Thousands place
+## puts (5639 % 1000) / 100 ##Hundreds place
+## puts (5639 % 100) / 10
+## puts (5639 % 10) / 1
+###
+
+#Ex3
+movies = { jaws: 1975,
+           anchorman: 2004,
+           man_of_steel: 2013,
+           a_beautiful_mind: 2001,
+           the_evil_dead: 1981 }
+
+puts movies[:jaws]
+
+#Ex4
+movies = [1975, 2004, 2013, 2001, 1981]
+puts movies[0]
+
+
+#Ex5
+puts 5*4*3*2
+
+#Ex6
+puts 1.2*1.2
---
+++ 101-programming-foundations/lesson_1/quiz.rb
@@ -0,0 +1,15 @@
+def string_lengths(sentence)
+  strings = sentence.split
+  lengths = []
+  counter = 1
+
+  until counter == strings.size do
+    word_length = strings[counter - 1].length
+    puts word_length
+    lengths.push(word_length)
+    counter += 1
+  end
+
+  lengths
+end
+string_lengths('To be or not to be')
