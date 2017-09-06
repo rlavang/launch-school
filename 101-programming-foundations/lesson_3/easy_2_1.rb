8f9807fcdf38c5dda54dfe371510e37d0f71ec74
InstantRamin <ramin@trisentry.com>
2017-09-06 16:39:55 -0400

    Added easy exercises

---
+++ 101-programming-foundations/lesson_3/easy_2_1.rb
@@ -0,0 +1,4 @@
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+p ages.key?("spot")
+p ages.include?("spot")
+p ages.member?("spot")
---
+++ 101-programming-foundations/lesson_3/easy_2_2.rb
@@ -0,0 +1,15 @@
+munsters_description = "The Munsters are creepy in a good way."
+munsters_description.capitalize!
+puts munsters_description
+
+munsters_description = "The Munsters are creepy in a good way."
+munsters_description.swapcase!
+puts munsters_description
+
+munsters_description = "The Munsters are creepy in a good way."
+munsters_description.downcase!
+puts munsters_description
+
+munsters_description = "The Munsters are creepy in a good way."
+munsters_description.upcase!
+puts munsters_description
