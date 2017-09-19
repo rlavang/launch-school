e5a42ecb7f0a177dc38d986f0356f3b8769935f6
Ramin Lavang <ramin@trisentry.com>
2017-09-19 07:49:38 -0400

    Create easy_2_8.rb

---
+++ 101-programming-foundations/small_problems/easy_2_8.rb
@@ -0,0 +1,26 @@
+def compute_sum(num)
+  (1..num).inject(:+)
+end
+
+def compute_product(num)
+  (1..num).inject(:*)
+end
+
+puts "Please enter an integer greater than 0:"
+num = gets.chomp.to_i
+
+loop do
+  puts "Enter 's' to compute the sum, 'p', to compute the product."
+  decision = gets.chomp
+  if decision == 's'
+    sum = compute_sum(num)
+    puts "The sum of the integers between 1 and #{num} is #{sum}."
+    break
+  elsif decision == 'p'
+    product = compute_product(num)
+    puts "The product of the integers between 1 and #{num} is #{product}."
+    break
+  else
+    puts "Unknown operation selected. Try again."
+  end
+end
