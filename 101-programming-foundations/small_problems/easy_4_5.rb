10bfbb2e63bb0d23351eaaaedb051cef9c701916
Ramin Lavang <ramin@trisentry.com>
2017-09-21 08:42:21 -0400

    Update easy_4_5.rb

--- 101-programming-foundations/small_problems/easy_4_5.rb
+++ 101-programming-foundations/small_problems/easy_4_5.rb
@@ -1,13 +1,20 @@
 def multiple?(num, divisor)
   num % divisor == 0
 end
-
+=begin
 def multisum(num)
   sum = 0
   1.upto num do |index|
     sum += index if multiple?(index, 3) || multiple?(index, 5) 
   end
   sum
+end
+=end
+
+def multisum(max_num)
+  (1..max_num).inject(0) do |sum, num|
+    multiple?(num, 3) || multiple?(num, 5) ? sum + num : sum
+  end
 end
 p multisum(3) == 3
 p multisum(5) == 8
