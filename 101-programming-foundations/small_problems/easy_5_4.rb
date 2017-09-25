cdc81e5f3ab7bf162fa313dbea003784354f3bed
Ramin Lavang <ramin@trisentry.com>
2017-09-25 11:01:39 -0400

    Update easy_5_4.rb

--- 101-programming-foundations/small_problems/easy_5_4.rb
+++ 101-programming-foundations/small_problems/easy_5_4.rb
@@ -11,3 +11,22 @@ end
 p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
 p swap('Abcde') == 'ebcdA'
 p swap('a') == 'a'
+
+=begin Further Exploration
+def swap_first_last_characters(a, b)
+  a, b = b, a
+end
+
+def swap(words)
+  result = words.split.map do |word|
+    a, b = swap_first_last_characters(word[0], word[-1])
+    word[0] = a
+    word[-1] = b
+    word
+  end
+  result.join(' ')
+end
+p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
+p swap('Abcde') == 'ebcdA'
+p swap('a') == 'a'
+=end
