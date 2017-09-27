3b6b8c5fe46934d3e6e61714ae476a81445e9065
Ramin Lavang <ramin@trisentry.com>
2017-09-27 08:29:55 -0400

    Update easy_5_2.rb

--- 101-programming-foundations/small_problems/easy_5_2.rb
+++ 101-programming-foundations/small_problems/easy_5_2.rb
@@ -1,3 +1,5 @@
+DAYS =  %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
+SUNDAY = Time.new(2017, 9, 24)
 def time_of_day(delta_minutes)
   delta_minutes = delta_minutes % (60 * 24)
   hours = delta_minutes / 60
@@ -5,10 +7,17 @@ def time_of_day(delta_minutes)
   format("%02d:%02d", hours, minutes)
 end
 
-p time_of_day(0) == "00:00"
-p time_of_day(-3) == "23:57"
-p time_of_day(35) == "00:35"
-p time_of_day(-1437) == "00:03"
-p time_of_day(3000) == "02:00"
-p time_of_day(800) == "13:20"
-p time_of_day(-4231) == "01:29"
+# Further Exploration
+def what_day(minutes_from_midnight)
+  time = SUNDAY + (minutes_from_midnight * 60)
+  "#{DAYS[time.wday]} #{'%02d' % time.hour}:#{'%02d' % time.min}"
+end
+
+p what_day(0) 
+p what_day(-3)
+p what_day(35)
+p what_day(-1437) 
+p what_day(3000) 
+p what_day(800) 
+p what_day(-4231) 
+
